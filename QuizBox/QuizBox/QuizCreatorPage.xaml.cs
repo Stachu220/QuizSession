5ab9a1397824c28ff2f27e14808709f3add906d6
Stachu220 <stachu2200@gmail.com>
2025-04-09 15:56:07 +0200

    started json file creator, will fix path later

--- QuizBox/QuizBox/QuizCreatorPage.xaml.cs
+++ QuizBox/QuizBox/QuizCreatorPage.xaml.cs
@@ -1,9 +1,48 @@
+using System.Text.Json;
+
 namespace QuizBox;
 
 public partial class QuizCreatorPage : ContentPage
 {
-	public QuizCreatorPage()
+	private string? quizTitle;
+    private string? quizDescription;
+    private string? path;
+
+    public QuizCreatorPage()
 	{
 		InitializeComponent();
 	}
+
+    private async void onAddQuestionsClicked(object sender, EventArgs e)
+    {
+        quizTitle = TitleEntry.Text;
+        quizDescription = DescriptionEntry.Text;
+
+        if (!string.IsNullOrEmpty(quizTitle) && !string.IsNullOrEmpty(quizDescription))
+        {
+            path = quizTitle + ".json";
+
+            var quiz = new
+            {
+                quiz = new[]
+                {
+                    new
+                    {
+                        name = quizTitle,
+                        description = quizDescription,
+                        questions = new List<object>()
+                    }
+                }
+            };
+            var json = JsonSerializer.Serialize(quiz, new JsonSerializerOptions { WriteIndented = true });
+            //TODO: Fix file path
+            File.WriteAllText(path, json);
+
+            await Shell.Current.GoToAsync("///QuestionCreatorPage");
+        }
+        else
+        {
+            await DisplayAlert("Error", "Please enter a title and description for the quiz.", "OK");
+        }
+    }
 }
--- QuizBox/QuizBox/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/QuizBox.AssemblyInfo.cs
+++ QuizBox/QuizBox/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/QuizBox.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizBox")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3791ecb9eb8823f446c0fdd6b9ff93eb96549d32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+cf66705ae25be94870b828105907c284aa8f7eb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizBox")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizBox")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
